2c5dbca4f2d7f42eec59f9bde6ff602eb2bdac56
Durgesh Samariya <samariya.durgesh@gmail.com>
2024-07-04 12:48:03 +1000

    import error fix.

--- app/db.py
+++ app/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.config import settings
+from config import settings
 
 # SQLAlchemy database engine and session
 engine = create_engine(settings.DATABASE_URL)
